84a31e4b80ad2f23351a5fce6c75b93ef8c0a330
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-10-08 22:57:21 +0200

    feat add basename to BrowserRouter to display the app properly

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/myreads'>
         <App />
     </BrowserRouter>,
     document.getElementById("root")
